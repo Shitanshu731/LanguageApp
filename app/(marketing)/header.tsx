aa00d3b95733078e7c28639998e3b263bad06937
Shitcoder731 <shitanshu.731@gmail.com>
2024-03-31 12:06:34 +0530

    Add Header with signUP and SignIn Button

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,10 +1,41 @@
+import { ClerkLoaded, ClerkLoading, SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
 import React from 'react'
+import {Loader} from "lucide-react"
+import { SignedIn, UserButton, SignInButton, SignedOut } from '@clerk/nextjs'
+import { Button } from '@/components/ui/button'
 
 const Header = () => {
   return (
-    <div className='fixed top-0 border-1'>
-      Header
-    </div>
+    <header className='h-20 w-full border-b-2 px-4 border-slate-200'>
+     <div className='lg:max-w-screen-lg mx-auto flex items-center justify-between h-full'>
+      <div className='pt-8 pl-4 pb-7 items-center gap-x-3 flex'>
+        <Image src="/mascot.svg" alt="Mascot" height ={40} width={40}/>
+        <h1 className='text-2xl font-extrabold text-green-600 tracking-wide'>
+          Language
+        </h1>
+      </div>
+      <ClerkLoading>
+        <Loader  className='h-5 w-5 text-muted-foreground animate-spin'/>
+      </ClerkLoading>
+      <ClerkLoaded>
+        <SignedIn>
+          <UserButton afterSignOutUrl='/'/>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton
+          mode="modal"
+          afterSignInUrl='/learn'
+          afterSignUpUrl='/learn'
+          >
+            <Button variant="ghost" size="lg">
+              Login
+            </Button>
+          </SignInButton>
+        </SignedOut>
+      </ClerkLoaded>
+      </div>
+    </header>
   )
 }
 
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -4,8 +4,8 @@ import Header from "./header"
 const MarketingLayout = ({children} : {children  : React.ReactNode}) =>{
     return ( 
         <div className="min-h-screen flex flex-col">
+            <Header />
             <main className="flex-1 flex flex-col items-center justify-center">
-                <Header />
                 {children}
             </main>
             <Footer />
