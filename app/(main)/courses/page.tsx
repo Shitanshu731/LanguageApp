fd66cdfd272d993b533a2ac933246c7ff365cc65
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-01 20:39:12 +0530

    Display the Courses data from the Database

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,11 +1,15 @@
+import { getCourses } from '@/db/query'
+import { List } from 'lucide-react'
 import React from 'react'
 
-const page = () => {
+const page = async() => {
+    const data = await getCourses();
   return (
     <div className='h-full max-w-[912px] px-3 mx-auto'>
         <h1 className='text-2xl font-bold text-neutral-700 '>
             Language Courses
         </h1>
+        {JSON.stringify(data)}
     </div>
   )
 }
