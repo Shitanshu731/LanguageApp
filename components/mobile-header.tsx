0e445af42a7826c17fb6b7c3357527e529b9925f
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-01 11:48:28 +0530

    Add Sheet Shadcn for Sidebar

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { MobileSidebar } from './mobile-sidebar'
 
 const MobileHeader = () => {
   return (
-    <nav className='md:hidden px-6 h-[50px] flex items-center bg-yellow-500 border-b fixed top-0 w-full z-50'>
-      Mobile Header
+    <nav className='md:hidden px-6 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50'>
+      <MobileSidebar />
     </nav>
   )
 }
