df45242365e68805b90ed9a49407f67c683fa13b
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-01 20:59:34 +0530

    Seed Card Component with the Schema data

--- app/(main)/courses/card.tsx
+++ app/(main)/courses/card.tsx
@@ -1,3 +1,6 @@
+import { cn } from '@/lib/utils';
+import { Check } from 'lucide-react';
+import Image from 'next/image';
 import React from 'react'
 type props ={
     title : string;
@@ -7,10 +10,22 @@ type props ={
     disabled?: boolean;
     active?: boolean;
 }
-const Card = ({} : props) => {
+const Card = ({title,imageSrc,id,disabled,active,onClick} : props) => {
   return (
-    <div>
-      Card
+    <div 
+    className={cn("h-full border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col items-center justify-between p-3 pb-6 min-h-[217px] min-w-[200px]",disabled && "pointer-events-none opacity-50")}
+    onClick={() => onClick(id)}
+    >
+        <div className='min-[24px] w-full flex tems-center justify-end'>
+            {active && (
+                <div className='rounded-md bg-green-600 flex items-center justify-center p-1.5'>
+                    <Check className='text-white stroke-[4] h-4 w-4' />
+
+                </div>
+            )}
+        </div>
+        <Image src={imageSrc} alt={title} height={70} width={93.3} className='rounded-lg drop-shadow-md border object-cover'/>
+        <p className='text-neutral-700 text-center font-bold mt-3'>{title}</p>
     </div>
   )
 }
