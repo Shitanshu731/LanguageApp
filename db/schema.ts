e6dd02f61ad421863181d9a93abba97ef5394326
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-02 12:04:25 +0530

    create a userProgress back relation with courses

--- db/schema.ts
+++ db/schema.ts
@@ -19,3 +19,10 @@ export const userProgress = pgTable("user_progress",{
     hearts : integer("hearts").notNull().default(5),
     points : integer("points").notNull().default(0)
 })
+
+export const userProgressRelations = relations(userProgress, ({one}) => ({
+    activeCourse : one(courses,{
+        fields : [userProgress.activeCourseId],
+        references : [courses.id]
+    })
+}))
