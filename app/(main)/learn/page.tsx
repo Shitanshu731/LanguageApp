1e24d5ca8953674e29ff3b41a3fb55266f85bf8e
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-02 12:24:48 +0530

    redirect to courses after signIn if not chosen any course

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -3,8 +3,12 @@ import { StickyWrapper } from "@/components/sticky-wrapper";
 import React from "react";
 import Header from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/query";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const userProgress = await getUserProgress();
+  if(!userProgress || !userProgress.activeCourseId) redirect("/courses")
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6 ">
       <StickyWrapper>
