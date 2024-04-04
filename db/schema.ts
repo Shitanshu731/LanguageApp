5989e1d15e7e3a77be68ff604209c0d7e10d0fcc
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-05 00:26:31 +0530

    Create a ChallengeProgress Schema

--- db/schema.ts
+++ db/schema.ts
@@ -72,6 +72,14 @@ export const challengeOptionsRelations = relations(challengeOptions, ({one}) => 
     })
 }))
 
+export const challengeProgress = pgTable("challengeProgress", {
+    id: serial("id").primaryKey(),
+    userId : text("user_id").notNull(),
+    challengeId : integer("challenge_id").references(() => challenges.id , {onDelete : "cascade"}).notNull(),
+    completed : boolean("completed").notNull().default(false),
+
+})
+
 export const coursesRelations = relations(courses, ({many}) => ({
     userProgress : many(userProgress),
 }))
