b2ebb277da1e7d07c50b9f8169b5aa3e005cb464
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-08 23:50:05 +0530

    Feature : add indentation Level to each lessons

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -8,10 +8,25 @@ type props ={
     percentage:  number;
 }
 
-export const LessonButton = ({}: props) => {
+export const LessonButton = ({id,index,totalCount,locked,current,percentage}: props) => {
+    const cycleLength = 8;
+    const cycleIndex = index % cycleLength;
+
+    let indentationLevel;
+
+    if(cycleIndex <= 2){
+        indentationLevel = cycleIndex;
+    }else if(cycleIndex <= 4){
+        indentationLevel = 4 - cycleIndex;
+    }else if(cycleIndex <= 6){
+        indentationLevel = 4-cycleIndex;
+    }else{
+        indentationLevel = cycleIndex - 8;
+    }
+
     return (
         <div>
-            
+             
         </div>
     )
 }
