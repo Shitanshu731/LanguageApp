55b5b9a3e6fad8b3e0a59a7369e492732434e977
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-09 00:18:53 +0530

    Add progressbart to lesson-button

--- app/(main)/learn/lesson-button.tsx
+++ app/(main)/learn/lesson-button.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Check, Crown, Star } from "lucide-react";
+import {CircularProgressbarWithChildren} from "react-circular-progressbar";
+import {cn} from "@/lib/utils";
+
 import Link from "next/link";
 
 type props ={
@@ -47,6 +51,31 @@ export const LessonButton = ({id,index,totalCount,locked,current,percentage}: pr
                         Start
                         <div className="absolute left=1/2 -bottom-2 w-0 h-0 border-x-8 border-x-transparent border-t-8 transform -translate-x-1/2" />
                         </div>
+                        <CircularProgressbarWithChildren value={Number.isNaN(percentage) ? 0 : percentage}
+                            styles={
+                                {
+                                    path :{ 
+                                        stroke : "#4ade80",
+                                    },
+                                    trail: {
+                                        stroke : "#e5e7eb",
+                                    },
+                                }
+                            }
+                        >
+                           <Button
+                           size="rounded"
+                           variant={locked ? "locked" : "secondary"}
+                           className="h-[70px] w-[70px] border-b-8"
+                           
+                           >
+                            <Icon className={cn(
+                                    "h-10 w-10",
+                                    locked ? "fill-neutral-400 text-neutral-400 stroke-neutral-400" : "fill-primary-foreground text-primary-foreground",isCompleted && "fill-none stroke-[4]"
+                                )}
+                            />
+                            </Button>  
+                        </CircularProgressbarWithChildren>
                     </div>
                 ) : (
                     <div> Something
