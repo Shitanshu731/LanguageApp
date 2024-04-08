2f79f4d13d23603e64a4733a98b94435df6166e9
Shitcoder731 <shitanshu.731@gmail.com>
2024-04-08 23:36:47 +0530

    Create lessonButton for learn page

---
+++ app/(main)/learn/lesson-button.tsx
@@ -0,0 +1,17 @@
+"use client";
+type props ={
+    id:number;
+    index:number;
+    totalCount : number;
+    locked?: boolean;
+    current? : boolean;
+    percentage:  number;
+}
+
+export const LessonButton = ({}: props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
